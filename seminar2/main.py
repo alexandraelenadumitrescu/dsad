d4292a7d26d89f6ee74c514e0c7b3b7a307d6bff
alexandraelenadumitrescu <134046146+alexandraelenadumitrescu@users.noreply.github.com>
2025-10-13 11:06:18 +0300

    sem 2

--- seminar2/main.py
+++ seminar2/main.py
@@ -10,3 +10,7 @@ t=pd.read_csv("data_in/Religious.csv",index_col=0)
 nan_replace_df(t)
 #print(t)
 confesiuni=list(t)[2:]
+
+coduri_localitati=pd.read_csv("data_in/Coduri_Localitati.csv",index_col=0)
+
+t_localitate=t[confesiuni].merge()
